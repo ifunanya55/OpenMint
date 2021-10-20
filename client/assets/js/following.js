3dac65e1568f3b8ea2468ac19c2bcb258bd8cc35
D3G3N <42593114+ifunanya55@users.noreply.github.com>
2021-10-21 00:26:22 +0100

    Update following.js

--- client/assets/js/following.js
+++ client/assets/js/following.js
@@ -1,5 +1,5 @@
-Moralis.initialize(""); // Application id from moralis.io
-Moralis.serverURL = ''; //Server url from moralis.io
+Moralis.initialize("AoMPSVgQKKY8qNgwNM087UN3pYP3du50NwmWnHtN"); // Application id from moralis.io
+Moralis.serverURL = 'https://undhzl8r7aym.usemoralis.com:2053/server'; //Server url from moralis.io
 
 const user = Moralis.User.current();
 
