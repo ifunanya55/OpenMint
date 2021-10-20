57f58e1f91ea995e0db0dd20d26ea1cae7c92a8d
D3G3N <42593114+ifunanya55@users.noreply.github.com>
2021-10-21 00:28:58 +0100

    Update connectWallet.js

--- client/assets/js/connectWallet.js
+++ client/assets/js/connectWallet.js
@@ -1,5 +1,5 @@
-Moralis.initialize("https://undhzl8r7aym.usemoralis.com:2053/server"); // Application id from moralis.io
-Moralis.serverURL = 'AoMPSVgQKKY8qNgwNM087UN3pYP3du50NwmWnHtN'; //Server url from moralis.io
+Moralis.initialize("AoMPSVgQKKY8qNgwNM087UN3pYP3du50NwmWnHtN"); // Application id from moralis.io
+Moralis.serverURL = 'https://undhzl8r7aym.usemoralis.com:2053/server'; //Server url from moralis.io
 
 $(document).ready(()=>{
   initUser();
